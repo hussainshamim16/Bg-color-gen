9e2147d0e250e26b10875606d7da3a7d2ca660f1
HUSSAIN SHAMIM <142653978+hussainshamim16@users.noreply.github.com>
2024-12-07 20:36:54 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+console.log("Hello World");
+let body = document.getElementById("body");
+
+function normal(){
+ let back = body.style.background = "none";
+ if(body === back){
+    alert("Body Is Already Normal")
+ }else{
+ let back = body.style.background = "none";
+
+ }
+}
+
+function red(){
+ body.style.backgroundColor = "red";
+}
+function yellow(){
+ body.style.backgroundColor = "yellow";
+}
+function green(){
+ body.style.backgroundColor = "green";
+}
+function pink(){
+ body.style.backgroundColor = "pink";
+}
+function brown(){
+ body.style.backgroundColor = "brown";
+}
+function grey(){
+ body.style.backgroundColor = "gray";
+}
+function black(){
+ body.style.backgroundColor = "black";
+}
+function orange(){
+ body.style.backgroundColor = "orange";
+}
+function blue(){
+ body.style.backgroundColor = "blue";
+}
+function darkslateblue(){
+ body.style.backgroundColor = "darkslateblue";
+}
+
+
+
+
+
